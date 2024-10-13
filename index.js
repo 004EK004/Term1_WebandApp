650e7dc33ecb41393d8379a4c4ef0ff40d13deab
004EK004 <97364755+004EK004@users.noreply.github.com>
2024-10-13 10:28:48 +0100

    Week 3 Task- Employee

---
+++ index.js
@@ -0,0 +1,10 @@
+// Define the Employeeinfo function
+function Employeeinfo(name, salary) {
+    console.log("Employee Name: " + name);
+    console.log("Employee Salary: $" + salary);
+}
+console.log("This is my first program")
+
+// Call the function with different values
+Employeeinfo("John Smith", 50000);
+
